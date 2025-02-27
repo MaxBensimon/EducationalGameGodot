35d1ebb3bc5d5c313fbcfad49426caeb2b75d7d9
Max <f_mb@live.dk>
2025-02-27 09:26:16 +0100

    yea

---
+++ Scripts/CameraControl.gd
@@ -0,0 +1,42 @@
+extends Camera3D
+
+var distance_from_player := 10
+var camera_sensitivity := .0025
+
+var yaw := 0.0
+var pitch := 0.0
+
+func _ready() -> void:
+		pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(-45))
+
+func _input(event): # LEFT MOUSE is only camera. RIGHT MOUSE is camera and player direction
+	if event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			yaw -= event.relative.x * camera_sensitivity
+			pitch -= event.relative.y * camera_sensitivity
+			pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(-45))
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+				snap_player_to_camera_direction()
+
+func _process(delta):
+	var player = get_parent()
+	
+	var target_position = player.global_transform.origin
+	var offset = Vector3(0, 0, distance_from_player)
+	offset = offset.rotated(Vector3.UP, yaw)
+	
+	var right = offset.cross(Vector3.DOWN).normalized()
+	offset= offset.rotated(right, pitch)
+	
+	global_transform.origin = target_position + offset
+	look_at(target_position, Vector3.UP)
+	
+func snap_player_to_camera_direction():
+	var player = get_parent()
+	var camera_forward = global_transform.basis.z
+	camera_forward.y = 0
+	camera_forward = camera_forward.normalized()
+	
+	var target_rotation = atan2(camera_forward.x, camera_forward.z)
+	
+	player.rotation.y = target_rotation
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,22 +1,26 @@
 extends CharacterBody3D
 
-var speed: float = 5.0
-var rotation_speed: float = 2.0
+var speed: float = 3
+var rotation_speed: float = 3
 
 func _physics_process(delta: float) -> void:
-	# Initialize movement direction
 	var direction = Vector3.ZERO
-
+	
 	if Input.is_action_pressed("ui_up"):
 		direction -= transform.basis.z
 	if Input.is_action_pressed("ui_down"):
 		direction += transform.basis.z
+	
+	if Input.is_action_pressed("ui_left") and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) or Input.is_action_pressed("ui_end"):
+		direction -= transform.basis.x
 	if Input.is_action_pressed("ui_left"):
 		rotate_y(rotation_speed * delta)
+	if Input.is_action_pressed("ui_right") and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) or Input.is_action_pressed("ui_page_down"):
+		direction += transform.basis.x
 	if Input.is_action_pressed("ui_right"):
 		rotate_y(-rotation_speed * delta)
-		
+	
 	direction = direction.normalized()
-
+	
 	velocity = direction * speed
 	move_and_slide()
